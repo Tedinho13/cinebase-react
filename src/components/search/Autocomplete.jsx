d8b1993fbc8225759e98ea6edcaaf1348ffdd61d
Dariusz Skrzypczak <Tedinho08@gmail.com>
2026-07-20 19:12:41 +0200

    bug fixes

--- src/components/search/Autocomplete.jsx
+++ src/components/search/Autocomplete.jsx
@@ -3,6 +3,8 @@ import './Autocompletion.css';
 const Autocomplete = ({activeItem, data, onClick}) => {
   
   const items = data.map((item, index) => <li className={`autocompletion__item ${index === activeItem ? "active" : ""}`} key={item.id} onClick={onClick}>{item.original_title}</li>);
+
+  if(items.length === 0) return;
   
   return <ul className="autocompletion__list">
     {items}
